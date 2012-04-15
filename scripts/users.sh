817f943d0aa6b6949f66501b16a5e09651b8b253
Brooke Kuhlmann <brooke@redalchemist.com>
2012-04-15 08:41:20 -0600

    Added more documentation to the scripts.

--- scripts/users.sh
+++ scripts/users.sh
@@ -4,9 +4,11 @@
 # Execution #
 #############
 
+# Create user account.
 useradd -m -s /bin/bash -G admin $USER_LOGIN
 usermod -p $(echo $USER_PASS | openssl passwd -1 -stdin) $USER_LOGIN
 
+# Initialize SSH authorized keys for user account.
 mkdir /home/$USER_LOGIN/.ssh
 touch /home/$USER_LOGIN/.ssh/authorized_keys
 chmod -R 700 /home/$USER_LOGIN/.ssh
--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@
 
 set -u # Exit if any variables are uninitialised.
 set -e # Exit if any command returns non-zero.
-export SERVER_IP=127.0.1.1 # Placeholder for registered IP.
-export SERVER_HOSTNAME=home.example.com # Placeholder for registered domain.
+export SERVER_IP=127.0.1.1 # Placeholder for server IP.
+export SERVER_HOSTNAME=home.example.com # Placeholder for server domain.
 export USER_LOGIN=app # The master user for automated tasks, deployments, app management, etc.
 export USER_PASS=password # The master user password.
 export INSTALL_COMMAND="apt-get -q -y"
