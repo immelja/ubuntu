09f34485ba354b4132a857e321b4ad95ac89c708
Brooke Kuhlmann <brooke@alchemists.io>
2014-11-09 11:07:12 -0700

    Added Bash strict mode.

--- settings/settings.sh
+++ settings/settings.sh
@@ -5,9 +5,10 @@
 
 # SETTINGS
 # General
-set -o nounset
-set -o errexit
-set -o pipefail
+set -o nounset # Exit, with error message, when attempting to use an undefined variable.
+set -o errexit # Abort script at first error, when a command exits with non-zero status.
+set -o pipefail # Returns exit status of the last command in the pipe that returned a non-zero return value.
+IFS=$'\n\t' # Defines how Bash splits words and iterates arrays. This defines newlines and tabs as delimiters.
 
 # Globals
 export SERVER_IP=127.0.1.1 # Placeholder for server IP.
